3bf77a370e8ac0e29ada03e17b14a75c03d6e200
akulais <akulais@gmail.com>
2016-03-14 16:22:24 -0700

    before presentation

--- project/__main__.py
+++ project/__main__.py
@@ -3,13 +3,13 @@
 from project_module import projects
 
 # main class will go here and be called everywhere else
-# class Queries(object):
-# 	# table1_name = ''
-# 	# table2_name = ''
-# 	# table3_name = ''
-# 	column1_name = ''
-# 	# column2_name = ''
-# 	# column3_name = ''
-# 	column_count = 0
+class Queries(object):
+	# table1_name = ''
+	# table2_name = ''
+	# table3_name = ''
+	column1_name = ''
+	# column2_name = ''
+	# column3_name = ''
+	column_count = 0
 
 	#talking to the settings
